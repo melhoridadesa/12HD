0f851fc1cae7dc610c3ac4997facd29f3fd8d5b5
melhoridadesa <zkttnjek@imgof.com>
2016-06-04 17:50:53 -0300

    Update layout.php

--- admin/html/layout.php
+++ admin/html/layout.php
@@ -12,9 +12,9 @@
 		</header>
 		<div id="menu">
 			<ul class="menu lista">
-				<li><a href="http://localhost/12hd/admin/index.php?page=menu">cadastrar menu</a></li>
-				<li><a href="http://localhost/12hd/admin/index.php?page=lista">lista de responsaveis</a></li>
-				<li><a href="http://localhost/12hd/admin/index.php?page=relatorio">Relatorio</a></li>
+				<li><a href="http://localhost/12HD/admin/html/layout.php?page=menu">cadastrar menu</a></li>
+				<li><a href="http://localhost/12HD/admin/html/layout.php?page=lista">lista de responsaveis</a></li>
+				<li><a href="http://localhost/12HD/admin/html/layout.php?page=relatorio">Relatorio</a></li>
 			</ul>
 		</div>
 		<content>
