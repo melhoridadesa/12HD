cbd8123110e740701d46a472aacf685f72242f74
Vitor Souza <root@php501.4linux.com.br>
2016-06-04 14:15:56 -0300

    Alterado funcao de conexao

--- admin/libs/conexao/db.php
+++ admin/libs/conexao/db.php
@@ -1,6 +1,6 @@
 <?php
 
-function ConexaoBD($host, $user, $pass, $banco)
+function ConexaoBD($host, $user, $pass, $banco, $sql)
 {
 
 	if (strlen($host) == 0 || $host == "*" || !is_string($host)) {
@@ -15,7 +15,12 @@ function ConexaoBD($host, $user, $pass, $banco)
 	elseif (strlen($banco) == 0 || $banco == "*" || !is_string($banco)) {
 		die("Por favor informar o banco de dados");
 	}
+	elseif (strlen($sql) == 0 || $sql == "*" || !is_string($sql)) {
+		die("Por favor informar a sql");
+	}
 	else{
-		return $conexao = mysqli_connect($host, $user, $pass, $banco) or die(mysqli_error());
+		$conexao = mysqli_connect($host, $user, $pass, $banco) or die(mysqli_error());
+		$resultado = mysqli_query($conexao, $sql);
+		return $resultado;
 	}
 }
