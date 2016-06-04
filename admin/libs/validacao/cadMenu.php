b1a7f37c7d80f4d8fd2f6ca82247a814437a8161
Vitor Souza <root@php501.4linux.com.br>
2016-06-04 17:49:36 -0300

    Inserindo a funcao para data e hora no cadastro de menu

--- admin/libs/validacao/cadMenu.php
+++ admin/libs/validacao/cadMenu.php
@@ -7,6 +7,6 @@ $titulo = $_POST['titulo'];
 $posicao = $_POST['posicao'];
 $corpo = $_POST['corpo'];
 $ativo = 1;
-$data = "2016/06/04";
+$data = date("Y-m-d H:i:s");
 
 CadastraMenu($link, $titulo, $corpo, $ativo, $data, $posicao);
