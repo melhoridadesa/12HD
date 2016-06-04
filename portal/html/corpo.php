24492abc52c8fd941288a86c0f03222a8c0e8cdd
Vitor Souza <root@php501.4linux.com.br>
2016-06-04 14:31:19 -0300

    estrutura do portal

---
+++ portal/html/corpo.php
@@ -0,0 +1,20 @@
+<?php
+
+if (isset($_GET['page']) && !empty($_GET['page']) ) {
+	
+
+	echo $_GET['page'];
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ portal/html/layout.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title>PORTAL DO IDOSO</title>
+		<style>
+			* {
+				padding: 0 auto;
+				margin: 0 auto;
+			}
+		</style>
+	</head>
+
+
+	<body>
+
+	
+		<table border="1" width="100%">
+			<thead>
+				<?php require_once "html/cabecalho.php"; ?>
+			</thead>
+			<tbody>
+				<tr><td></td>
+					<td><?php require_once "html/corpo.php"; ?></td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr >
+					<td colspan= "3" ><?php require_once "html/rodape.php"; ?></td>
+				</tr>
+			</tfoot>
+		</table>
+	</body>
+</html>
