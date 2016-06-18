48a9034b1247ea7a16e2617a05b37811f19cad6b
Vitor Souza <root@php501.4linux.com.br>
2016-06-18 12:32:38 -0300

    session

--- admin/html/content.php
+++ admin/html/content.php
@@ -1,5 +1,10 @@
 <?php 
-
+	session_start();
+	if(!isset($_SESSION['user'])){
+		header("Location:/12HD/admin/html/layout.php?page=lista");
+	}
+	
+	
 	$page = (isset($_GET['page'])) ? $_GET['page'] : 'menu';
 	
 	$file = (file_exists("pages/{$page}.php")) ? "pages/{$page}.php" : null;
