8f9e0df86f18c0ac3555579f9c30e49fafeba2f1
gabriel.fabrega@yahoo.com.br <gabriel.fabrega@yahoo.com.br>
2016-06-18 14:15:18 -0300

    Removendo if do corpo

--- portal/html/corpo.php
+++ portal/html/corpo.php
@@ -1,34 +1,24 @@
 <?php
-	$param = '';
-	if (isset($_GET['page']) && !empty($_GET['page']) && is_numeric($_GET['page']) && ($_GET['page']==3 || $_GET['page']==4)){
-		if ($_GET['page']==3)
+$param = '';
+if (isset($_GET['page']) && !empty($_GET['page']) && is_numeric($_GET['page']) && ($_GET['page']==3 || $_GET['page']==4)){
+	if ($_GET['page']==3)
 
-			if(file_exists('/var/www/12HD/admin/libs/crud/cad_responsavel.php')){
+		if(file_exists('/var/www/12HD/admin/libs/crud/cad_responsavel.php')){
 			require "/var/www/12HD/admin/libs/crud/cad_responsavel.php";}
 			else{
 				echo 'Pagina nao existe';
 			}
+
+		}else{
+			if(isset($_GET['page']) && !empty($_GET['page']) && is_numeric($_GET['page']))
+				$param = 'and id='. $_GET['page'];
+			else 
+				$param = '';
 			
-			$responsavel = "/var/www/12HD/admin/libs/crud/cad_responsavel.php";
-			if(isset(require $responsavel)){
-				require_once $responsavel;
-				echo "teste";
-			}
-
-			else{
-
-			}
-
-	}else{
-		if(isset($_GET['page']) && !empty($_GET['page']) && is_numeric($_GET['page']))
-			$param = 'and id='. $_GET['page'];
-		else 
-			$param = '';
-		
-		$resultado = ConexaoBD('localhost', 'root', '123456', 'MelhorIdadeSA', 'select titulo,corpo from Pagina where ativo=1 '. $param .' order by posicao asc, datahoraupdate desc limit 1');
-		$linha = mysqli_fetch_array($resultado,MYSQLI_ASSOC);
-		echo '<h1>'.$linha['titulo'].'</h1>';
-		echo '</br>';
-		echo $linha['corpo'];
-	}
-?>
+			$resultado = ConexaoBD('localhost', 'root', '123456', 'MelhorIdadeSA', 'select titulo,corpo from Pagina where ativo=1 '. $param .' order by posicao asc, datahoraupdate desc limit 1');
+			$linha = mysqli_fetch_array($resultado,MYSQLI_ASSOC);
+			echo '<h1>'.$linha['titulo'].'</h1>';
+			echo '</br>';
+			echo $linha['corpo'];
+		}
+		?>
